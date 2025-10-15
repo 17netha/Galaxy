b9b4399f6aaf603dd12f4d7582b7854f16faa01a
Nithin Ale <nithin-ale@epam.com>
2025-10-15 13:43:34 +0530

    fix(compo): added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: 'about', component: AboutComponent},
+  {path: '**', component: NotfoundComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
